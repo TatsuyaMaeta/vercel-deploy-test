20003b6b5250f9cb87a1b1a0e3149a903f18f499
TatsuyaMaeta <tat.mae084@gmail.com>
2022-06-01 17:49:12 +0900

    add select tag

--- src/components/ApiFetch.js
+++ src/components/ApiFetch.js
@@ -1,26 +1,33 @@
+// https://omathin.com/react-api/
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const ApiFetch = () => {
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         axios
-            .get("https://sleepy-journey-90647.herokuapp.com/", {
+            .get("https://sleepy-journey-90647.herokuapp.com", {
                 mode: "cors",
             })
             .then((res) => {
                 setPosts(res.data.items);
             });
-    },[])
+    }, []);
     return (
         <div>
             <h2>沖縄料理屋をAPIで取ってきてみた</h2>
             <ul>
                 {posts.map((post) => (
-                    <li> {post.name} </li>
+                    <li key={post.id}> {post.name} </li>
                 ))}
             </ul>
+            <select>
+                {posts.map((post) => (
+                    <option key={post.id}>{post.name}</option>
+                ))}
+            </select>
             <div>Gs academy test at Vercel_</div>
         </div>
     );
